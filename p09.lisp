c952e277a2eaf227843345cdfeab3e8c2e7423c1
John Mastro <john.b.mastro@gmail.com>
2013-04-22 21:10:05 -0700

    Problem 9

---
+++ p09.lisp
@@ -0,0 +1,51 @@
+;; Problem 9 -------------------------------------------------------------------
+
+;; Pack consecutive duplicates of list elements into sublists.
+;; (This is more commonly known as `group-by` or `partition-by`).
+
+(defun pack-by/iter (list &key (test #'eql))
+  (flet ((iter (list)
+           (let ((result (list (list (car list)))))
+             (dolist (item (cdr list))
+               (if (funcall test item (caar result))
+                   (push item (car result))
+                   (push (list item) result)))
+             (nreverse result))))
+    (cond ((null list) list)
+          ((null (cdr list)) (list list))
+          (t (iter list)))))
+
+(defun pack-by/rec (list &key (test #'eql))
+  (labels ((rec (item group list)
+             (cond ((null list) (list (cons item group)))
+                   ((funcall test item (car list))
+                    (rec item (cons item group) (cdr list)))
+                   (t (cons (cons item group)
+                            (rec (car list) nil (cdr list)))))))
+    (cond ((null list) list)
+          ((null (cdr list)) (list list))
+          (t (rec (car list) nil (cdr list))))))
+
+;; This is almost identcal do the `dolist` version but I haven't
+;; used `loop` for any of these problems yet so I'll do it anyway.
+(defun pack-by/loop (list &key (test #'eql))
+  (flet ((run-loop (list)
+           (loop with result = (list (list (car list)))
+                 for item in (cdr list) do
+                 (if (funcall test item (caar result))
+                     (push item (car result))
+                     (push (list item) result))
+                 finally (return (nreverse result)))))
+    (cond ((null list) list)
+          ((null (cdr list)) (list list))
+          (t (run-loop list)))))
+
+(deftest test-pack-by ()
+  (check-each (pack-by/iter pack-by/rec pack-by/loop)
+    (equal (funcall it nil) nil)
+    (equal (funcall it '(a)) '((a)))
+    (equal (funcall it '(a a)) '((a a)))
+    (equal (funcall it '(a b)) '((a) (b)))
+    (equal (funcall it '(a a a a b c c a a d e e e e))
+           '((a a a a) (b) (c c) (a a) (d) (e e e e)))))
+
